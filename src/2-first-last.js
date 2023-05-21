bc3cb2710bad7b39cd567a3e541b93f10713e428
Sammy Olsson <sammy.olsson@gmail.com>
2023-05-21 19:00:10 +0200

    1-7 Done

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,13 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+  if (
+    items.length === 1
+  ) {
+  return `Only item: ${items[0]}`
+  } else if (
+    items.length >= 1) {
+    return `First: ${items[0]}, Last: ${items.slice(-1)}`
+  } else if (!items.length) {
+    return `No items!`
+  }
+  };
+
--- src/4-angle-calculator.js
+++ src/4-angle-calculator.js
@@ -1,3 +1,3 @@
 export const angleCalculator = (turns) => {
-  return
-}
+  return turn * 360
+};
